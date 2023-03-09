b817f8837528c71dda3f9e0e321f7b8518500b44
Zurab <chanishvili@gmail.com>
2023-03-09 16:34:38 +0400

    first commit

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from tkinter import *
 
 
 
-
-
-def enter(*args):
-   # test = entry.get()
-   canvas.itemconfig(answe_text, text = f'{var.get()} has {len(var.get())} characters')
+def enter():
+   test = entry.get()
+   canvas.itemconfig(answe_text, text = f'{test} has {len(test)} characters')
 
 
 
@@ -18,14 +16,14 @@ window.configure(padx=20, pady=10)
 
 
 canvas = Canvas(width=200, height=200)
-answe_text = canvas.create_text(100,130, text ='00')
+answe_text = canvas.create_text(100,130, text ='String Char Length')
 canvas.grid(column=1, row=0)
-var = StringVar()
+
 
 lable_text = Label(text="Enter Text").grid(row=0,column=0)
 
 
-# button_ent = Button(text="OK", command = enter).grid(row=0,column=2)
+button_ent = Button(text="OK", command = enter).grid(row=0,column=2)
 
 
 entry = Entry(window)
@@ -33,5 +31,5 @@ entry = Entry(window)
 entry.grid(row=0, column=1, padx=10)
 
 
-var.trace('w', enter)
+
 window.mainloop()
