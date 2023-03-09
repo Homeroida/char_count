ac9c24392a096768179cae815baea507d9e35efb
Zurab <chanishvili@gmail.com>
2023-03-10 00:23:30 +0400

    first commit

--- main.py
+++ main.py
@@ -2,10 +2,24 @@ from tkinter import *
 
 
 
-def enter():
-   test = entry.get()
-   canvas.itemconfig(answe_text, text = f'{test} has {len(test)} characters')
 
+
+
+
+def update_label(*args):
+   # Get the value of the Entry widget
+   string_value = var.get()
+
+   # Get the length of the string
+   string_length = len(string_value)
+
+   # Update the label with the length of the string
+   label.config(text=f"Dynamic character counter : {string_length}")
+
+
+def enter():
+   string_value = var.get()
+   canvas.itemconfig(answe_text,text=f'{string_value} has {len(string_value)}  characters.')
 
 
 #----------------------UI setup----------------------------
@@ -14,22 +28,31 @@ window = Tk()
 window.title("String Counter")
 window.configure(padx=20, pady=10)
 
+var = StringVar()
+
+
+
+
 
 canvas = Canvas(width=200, height=200)
 answe_text = canvas.create_text(100,130, text ='String Char Length')
+
 canvas.grid(column=1, row=0)
 
 
 lable_text = Label(text="Enter Text").grid(row=0,column=0)
 
 
+label = Label(window, text="Dynamic character counter : 0")
+label.place(x=90,y=40)
+
 button_ent = Button(text="OK", command = enter).grid(row=0,column=2)
 
 
-entry = Entry(window)
+entry = Entry(window,textvariable=var)
 
 entry.grid(row=0, column=1, padx=10)
 
-
+var.trace("w", update_label)
 
 window.mainloop()
